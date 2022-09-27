55fba2ed2bd051614d9eb78bfe01e894ed15152e
redyandri <redyandri@gmail.com>
2022-09-27 17:43:44 +0700

    fix empty respond for non existent dataset

--- dupakq_fe/src/Table.js
+++ dupakq_fe/src/Table.js
@@ -1,15 +1,101 @@
 import React from "react";
 import { useTable, useSortBy } from 'react-table';
 import { Navigate } from 'react-router';
+import { Link } from '@mui/material';
+import { useMemo } from "react";
 
 
 
 
-export default function Table({ columns, data }) {
+export default function Table({ data,ref0 }) {
     
   const defaultColumn = {
     width: "auto",
   }
+
+
+  const handleDownloadClick= async (idx)=>{
+    let arr=idx.split("_")
+    let idk=parseInt(arr[0], 10)
+    let kode=arr[2]
+    console.log("gonna download:",idk);
+
+     await fetch('http://127.0.0.1:8000/download/', {
+      mode:"cors",
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        "Content-type": "application/json"
+      },
+      // credentials: "include",
+      body: JSON.stringify({
+        "idx": idk,
+        "act":ref0.current.value,
+      })
+    }).then((res) => {
+      console.log(res)
+      return res.blob();
+  })
+  .then((blob) => {
+      const href = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = href;
+      let doc_name=kode +"_"+ref0.current.value+".docx"
+      link.setAttribute('download', doc_name); //or any other extension
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+  })
+  .catch((err) => {
+      return Promise.reject({ Error: 'Something Went Wrong', err });
+  })
+  };
+
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Kode",
+        accessor: "code",
+      },
+      {
+        Header: "Butir Kegiatan",
+        accessor: "activity",
+        maxWidth: 800,
+        minWidth: 350,
+        width: 350,
+      },
+      {
+        Header: "Angka Kredit",
+        accessor: "credit",
+      },
+      {
+        Header: "Jenjang",
+        accessor: "level",
+      },
+      {
+        Header: "",
+        accessor: "index",
+        Cell: ({ cell }) => (
+          cell.value!=-1 &&
+            <Link
+            component="button"
+            variant="body2"
+            onClick={() => {
+              handleDownloadClick(cell.value)
+            }}
+          >
+            download
+            </Link>
+          
+            
+          
+          
+        )     }
+     
+    ],
+    []
+  );
 
     // Use the useTable Hook to send the columns and data to build the table
     const {
