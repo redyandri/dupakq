9a3dea3b7939826a11dd293e026e44e36feaf4c7
redyandri <redyandri@gmail.com>
2022-10-18 17:27:51 +0700

    add graph

--- dupakq_fe/src/App.js
+++ dupakq_fe/src/App.js
@@ -6,6 +6,11 @@ import {useRef, useEffect} from 'react';
 import { useMemo } from "react";
 import Table from "./Table";
 import { Link } from '@mui/material';
+import { NeoGraph, ResponsiveNeoGraph } from "./NeoGraph";
+
+const NEO4J_URI = "bolt://10.242.184.93:7687";
+const NEO4J_USER = "neo4j";
+const NEO4J_PASSWORD = "test";
 
 
 function App() {
@@ -110,7 +115,24 @@ const empty_result={
         <Table data={data} ref0={ref0}/>
       
       </div>
-      {/* <List  input={inputText} /> */}
+      <div className="App" style={{ fontFamily: "Quicksand" }}>
+      <h1>React Neovis Example</h1>
+      <ResponsiveNeoGraph
+        containerId={"id0"}
+        neo4jUri={NEO4J_URI}
+        neo4jUser={NEO4J_USER}
+        neo4jPassword={NEO4J_PASSWORD}
+      />
+      <NeoGraph
+        width={600}
+        height={500}
+        containerId={"id1"}
+        neo4jUri={NEO4J_URI}
+        neo4jUser={NEO4J_USER}
+        neo4jPassword={NEO4J_PASSWORD}
+        backgroundColor={"#b2beb5"}
+      />
+    </div>
     </div>
   );
 }
