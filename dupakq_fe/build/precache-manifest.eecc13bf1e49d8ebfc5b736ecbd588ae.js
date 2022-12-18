280eb303746b97a16fc1fcb4c680b66365f7743a
redyandri <redyandri@gmail.com>
2022-12-18 23:22:53 +0700

    add build folder for frontend :D

---
+++ dupakq_fe/build/precache-manifest.eecc13bf1e49d8ebfc5b736ecbd588ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a72c1db0620e1ab83802",
+    "url": "/static/js/0.ca6750ec.chunk.js"
+  },
+  {
+    "revision": "6174b15e212abc38feed",
+    "url": "/static/css/main.0e149778.chunk.css"
+  },
+  {
+    "revision": "6174b15e212abc38feed",
+    "url": "/static/js/main.3b09d478.chunk.js"
+  },
+  {
+    "revision": "04ed325e64400775e26d",
+    "url": "/static/js/runtime~main.8f33d456.js"
+  },
+  {
+    "revision": "bf38ba943d0ca200d461",
+    "url": "/static/js/3.b59714bf.chunk.js"
+  },
+  {
+    "revision": "2c4197929070c0de41ef",
+    "url": "/static/js/4.d6b5dbad.chunk.js"
+  },
+  {
+    "revision": "32c99d65a396cb5486b2",
+    "url": "/static/js/5.a8aa6144.chunk.js"
+  },
+  {
+    "revision": "331a2880c6484648f93d",
+    "url": "/static/js/6.49f89595.chunk.js"
+  },
+  {
+    "revision": "17dcf896305055bb18b3",
+    "url": "/static/js/7.3e57361a.chunk.js"
+  },
+  {
+    "revision": "50f9e9844506e92cfbac9c94e102b6cd",
+    "url": "/static/media/apple3.50f9e984.png"
+  },
+  {
+    "revision": "3d9e20d3a4eb2a065e62e78da1e41b82",
+    "url": "/index.html"
+  }
+];
